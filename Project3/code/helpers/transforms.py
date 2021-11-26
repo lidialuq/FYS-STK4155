b31a74f19e3ac009f9e9325138e77a953e52202c
lidialuq <72806975+lidialuq@users.noreply.github.com>
2021-11-26 15:08:13 +0100

    Update transforms.py

--- Project3/code/helpers/transforms.py
+++ Project3/code/helpers/transforms.py
@@ -1,15 +1,3 @@
-# Copyright 2020 MONAI Consortium
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#     http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 from monai.apps import DecathlonDataset
@@ -43,7 +31,6 @@ from monai.transforms import (
     AddChanneld,
 )
 from monai.utils import set_determinism
-
 
 set_determinism(seed=99)
 root_dir = '/home/lidia/CRAI-NAS/BraTS/BraTs_2016-17'
